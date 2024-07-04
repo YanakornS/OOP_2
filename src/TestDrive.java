4f622cadb994785a307ff6d687b6c327b37a2f08
YanakornS <654259002@webmail.npru.ac.th>
2024-07-04 12:03:45 +0700

    first commit

---
+++ src/TestDrive.java
@@ -0,0 +1,16 @@
+import com.npru.se65.id65002.HumanBeing;
+
+
+
+public class TestDrive {
+
+	public static void main(String[] args) {
+		HumanBeing hb = new HumanBeing();
+		hb.setHeight(175);
+		hb.setWeight(72);
+		hb.calculateBMI();
+		System.out.println("BMI : "+hb.getBmi());
+		System.out.println(hb.analyzeBmi());
+	}
+
+}
---
+++ src/com/npru/se65/id65002/BmiIndexer.java
@@ -0,0 +1,10 @@
+package com.npru.se65.id65002;
+
+public class BmiIndexer {
+
+		public double calculateBMI(double height, double weight) {
+			return  (weight/(height*height))*10000;
+			
+		}
+		
+}
